ce63a6cfeb58f535597d0b1a169d6e66f7be52d0
ふじ <fjk.develop@gmail.com>
2025-08-11 00:33:22 +0900

    Project CRUD 完成段階 (#7)

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,11 +1,12 @@
 from .pagination import PaginatedResponse, Pagination
-from .project import ProjectCreate, ProjectDetailRead, ProjectRead
+from .project import ProjectCreate, ProjectDetailRead, ProjectRead, ProjectUpdate
 from .project_excel_format import ProjectExcelFormatRead
 
 __all__ = [
     "ProjectRead",
     "ProjectDetailRead",
     "ProjectCreate",
+    "ProjectUpdate",
     "ProjectExcelFormatRead",
     "PaginatedResponse", 
     "Pagination",
