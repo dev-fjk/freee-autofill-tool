d3b11d616750274a4e4ecb53a89634967f80b96b
dev-fjk <fjk.develop@gmail.com>
2025-08-11 17:19:01 +0900

    feat: 初期設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/freee-autofill-tool/", // ルートパスを指定
+    plugins: [react()],
+    server: {
+        port: 3000, // 開発サーバーのポート指定（任意）
+    },
+    resolve: {
+        alias: {
+            "@": "/src", // パスエイリアス（任意）
+        },
+    },
+});
